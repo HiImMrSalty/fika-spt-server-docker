02e0b6afdf729ec0acdf0ac277f948f5ba519bb8
Jason <jasonzareski@gmail.com>
2025-08-21 14:28:07 -0400

    fix some bash mishaps
    fixing a bug that would cause server to restart every cron run, even with no json files present in the mounted dir for update.

--- scripts/profile_updates.sh
+++ scripts/profile_updates.sh
@@ -7,37 +7,37 @@ DST="/opt/server/user/profiles"
 exec 9>/tmp/sync-profiles.lock
 flock -n 9 || exit 0
 
-updated=""
-find "$SRC" -type f -name "*.json" -print0 | while IFS= read -r -d "" f;
-do
-    timestamp=$(date +%Y%m%dT%H%M)
-	# Move old profile to a ".bak" with timestamp
-    base="$(basename "$f")"
-    dst="${DST}/${base}"
-	if [ -f "$dst" ];
-	then
-		backup_prof="${dst}-${timestamp}.bak"
-	    echo "backup ${base} to ${backup_prof}"
-		mv -f "${dst}" "${backup_prof}"
-	fi
-	
-	tmp="${dst}.tmp.$$"
-	echo "copy ${f} to ${tmp}, then move ${tmp} to ${dst}"
-    cp -f "$f" "$tmp" && mv -f "$tmp" "$dst"
-	
-	rm -f "$f"
-	updated="true"
-done
+updated=0
 
-if [[ "$updated" -eq "true" ]]; then
+# NOTE: use process substitution to keep the loop in the current shell
+while IFS= read -r -d '' f; do
+  echo "Checking ${f}"
+  timestamp="$(date +%Y%m%dT%H%M)"
+  base="$(basename "$f")"
+  dst="${DST}/${base}"
+
+  if [ -f "$dst" ]; then
+    backup_prof="${dst}-${timestamp}.bak"
+    echo "backup ${base} to ${backup_prof}"
+    mv -f "$dst" "$backup_prof"
+  fi
+
+  tmp="${dst}.tmp.$$"
+  echo "copy ${f} to ${tmp}, then move ${tmp} to ${dst}"
+  cp -f "$f" "$tmp" && mv -f "$tmp" "$dst"
+
+  rm -f "$f"
+  updated=1
+done < <(find "$SRC" -type f -name "*.json" -print0)
+
+if (( updated )); then
   echo "Profiles updated; requesting server restart..."
-  pids="$(pgrep -f 'SPT\.Server\.exe' || true)" 
-  if [ -n "$pids" ];
-  then
-      echo "Send SIGTERM to ${pids}"
-	  kill -TERM $pids || true
+  pids="$(pgrep -f 'SPT\.Server\.exe' || true)"
+  if [ -n "$pids" ]; then
+    echo "Send SIGTERM to ${pids}"
+    kill -TERM $pids || true
   else
-      echo "SPT.Server.exe not found (already down?)"
+    echo "SPT.Server.exe not found (already down?)"
   fi
 else
   echo "No updates"
